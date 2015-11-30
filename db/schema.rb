995741d47ca74c1425112bde8833290963d18e84
Your NameAyinde <ayinde@theupnext.com>
2015-11-30 22:03:17 +0000

    Added contact page to nav bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151109220903) do
-
-  create_table "contacts", force: true do |t|
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
